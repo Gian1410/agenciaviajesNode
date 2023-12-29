005ff40a650f5141bbfa152e6b394759018de1a2
Gian <giancastrovilca@gmail.com>
2023-12-29 15:44:42 -0500

    changes database

--- config/db.js
+++ config/db.js
@@ -3,11 +3,10 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const db = new Sequelize(process.env.DB_URL,{
-    // process.env.DB_NAME, process.env.DB_USER,process.env.DB_PASS,
-    // host: process.env.DB_HOST,
-    // port: '3306',
-    // dialect: 'mysql',
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER,process.env.DB_PASS,{
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
     define: {
         timestamps: false
     },
